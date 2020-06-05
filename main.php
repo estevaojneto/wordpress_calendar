cd85fc9f6121d5d7e6c5e9ac83f8f119832a4b7b
estevaojneto <estevao.jadanhi@gmail.com>
2020-06-05 15:40:27 -0400

    Created class, page, menu item

--- main.php
+++ main.php
@@ -7,20 +7,52 @@
  */
 defined( 'ABSPATH' ) or die( 'This plugin must not be directly accessed. Halting.' );
 
-final class BEC{
-    public function __construct() {
+final class BEC
+{
+	private $pluginPath;
+	private $pluginPages;
+	
+	public function __construct() {
+		$this->setConfig();
 		$this->setHooks();
     }
-
-	private function setHooks(){
-		//Actions:
-		add_action( 'the_content', array( $this, 'showCalendar' ) );
-		//Filters:
+	
+	private function setConfig(){
+		$this->pluginPath = dirname(__FILE__);
+		$this->pluginPages = $this->pluginPath . '/pages';
+		return;
 	}
+
+    private function setHooks(){
+        //Actions:
+        add_action( 'the_content', array( $this, 'showCalendar' ) );
+
+        //Adds a menu item:
+        add_action( 'admin_menu', array( $this, 'createMenuItem' ) );
+
+        //Filters:
+        return;
+    }
+	
 	public function showCalendar ( $content ) {
-    	return $content .= '<input type="date"/>'; // if it shows a calendar input, it's working
+        return $content .= '<input type="date"/>';
+	}
+
+    public function createMenuItem(){
+        $page_title = 'BEC - Events';   
+		$menu_title = 'BEC - Events';   
+		$capability = 'manage_options';
+		$menu_slug  = 'bec_settings';
+		$function   = array($this,'createSettingsPage');
+		$icon_url   = 'dashicons-media-code';   
+		add_menu_page( $page_title , $menu_title , $capability , $menu_slug , $function , $icon_url );
+	}
+	
+	public function createSettingsPage(){
+		require_once($this->pluginPages . '/settings.php' );
+		return;
 	}
 	
 }
-
+	
 $GLOBALS['BEC'] = new BEC();
---
+++ pages/settings.php
@@ -0,0 +1,3 @@
+<h1>BEC - Basic Event Calendar</h1>
+
+<p>Developed by Estevao Jadanhi Neto.</p>
