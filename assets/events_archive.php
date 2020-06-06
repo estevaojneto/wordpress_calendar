020fac0d3d9f6530b6cbad474149a66895179f41
estevaojneto <estevao.jadanhi@gmail.com>
2020-06-06 06:16:57 -0400

    Changed file structure. Added taxonomy.

---
+++ assets/events_archive.php
@@ -0,0 +1,7 @@
+<?php
+get_header();
+?>
+Events should go here
+<?php
+get_footer();
+?>
--- main.php
+++ main.php
@@ -6,11 +6,13 @@
  * Author: Estevao Jadanhi Neto
  */
 defined( 'ABSPATH' ) or die( 'This plugin must not be directly accessed. Halting.' );
+// Register Custom Post Type
+include_once('libraries/meta-box/meta-box.php');
 
 final class BEC
 {
 	private $pluginPath;
-	private $pluginPages;
+	private $pluginAssets;
 	
 	public function __construct() {
 		$this->setConfig();
@@ -19,38 +21,113 @@ final class BEC
 	
 	private function setConfig(){
 		$this->pluginPath = dirname(__FILE__);
-		$this->pluginPages = $this->pluginPath . '/pages';
+		$this->pluginPages = $this->pluginAssets . '/assets';
 		return;
 	}
 
     private function setHooks(){
         //Actions:
-        add_action( 'the_content', array( $this, 'showCalendar' ) );
-
-        //Adds a menu item:
-        add_action( 'admin_menu', array( $this, 'createMenuItem' ) );
-
-        //Filters:
+		//Create a custom post type ("events")
+		add_action( 'init', array( $this, 'eventPostType' ) );		
+		//Filters:
+        //Adds custom metaboxes using Metabox.io
+        add_filter( 'rwmb_meta_boxes', array( $this, 'eventMetaBoxes' ));
+		add_filter( 'page_template', array( $this, 'createEventArchive' ) );
         return;
     }
 	
-	public function showCalendar ( $content ) {
-        return $content .= '<input type="date"/>';
-	}
-
-    public function createMenuItem(){
-        $page_title = 'BEC - Events';   
-		$menu_title = 'BEC - Events';   
-		$capability = 'manage_options';
-		$menu_slug  = 'bec_settings';
-		$function   = array($this,'createSettingsPage');
-		$icon_url   = 'dashicons-media-code';   
-		add_menu_page( $page_title , $menu_title , $capability , $menu_slug , $function , $icon_url );
+	public function createEventArchive(){
+		if ( is_page( 'events_archive' ) ) {
+			$page_template = $this->pluginAssets . '/events_archive.php';
+		}
+		return $page_template;
 	}
 	
-	public function createSettingsPage(){
-		require_once($this->pluginPages . '/settings.php' );
-		return;
+	public function eventPostType(){
+		$labels = array(
+			'label'           => __('Events'),
+			'name'               => __( 'Events' ),
+			'singular_name'      => __( 'Event' ),
+			'add_new'            => __( 'Add New Event' ),
+			'add_new_item'       => __( 'Add New Event' ),
+			'edit_item'          => __( 'Edit Event' ),
+			'new_item'           => __( 'Add New Event' ),
+			'view_item'          => __( 'View Event' ),
+			'search_items'       => __( 'Search Event' ),
+			'not_found'          => __( 'No events found' ),
+			'not_found_in_trash' => __( 'No events found in Trash' )
+		);
+
+		$supports = array(
+			'title',
+			'editor',
+			'thumbnail',
+			'comments',
+			'revisions',
+		);
+
+		$args = array(
+			'labels'               => $labels,
+			'supports'             => $supports,
+			'public'               => true,
+			'capability_type'      => 'post',
+			'rewrite'              => array( 'slug' => 'events' ),
+			'has_archive'          => true,
+			'menu_icon'            => 'dashicons-calendar-alt',
+			'register_meta_box_cb' => 'eventMetaBoxes',
+		);
+		
+		register_taxonomy('events-category', 'events', array(
+			'hierarchical'    => true,
+			'label'           => __('Events'),
+			'query_var'       => 'events-category',
+			'rewrite'         => array('slug' => 'events' ),
+			)
+		);
+		
+		register_post_type( 'events', $args );
+		
+	}
+	public function eventMetaBoxes(){
+	    $meta_boxes[] = array(
+        'title' => 'Event Information',
+		'post_types' => 'events',
+        'fields' => array(
+            array(
+                'id' => 'start_date',
+                'name' => 'Starting date & time',
+				'required'  => true,
+				'type' => 'datetime'
+            ),
+			array(
+                'id' => 'end_date',
+                'name' => 'End date',
+				'required'  => false,
+				'type' => 'datetime'
+            ),
+			array(
+				'name'            => 'Recurrency',
+				'desc'	=> 'Selecting this setting will do stuff TODO',
+				'id'              => $prefix,
+				'type'            => 'select',
+				'options'         => array(
+					'0'       => 'Single-time event',
+					'1' => 'Every day',
+					'7'        => 'Every week',
+					'30'     => 'Every month',
+				),
+				'multiple'        => false,
+				'placeholder'     => '',
+				'select_all_none' => true,
+			),
+			array(
+				'id'   => 'external_link',
+				'type' => 'url',
+				'name' => 'External Link (ie. event website)',
+			),
+		),
+    );
+    return $meta_boxes;	
 	}
 	
 }
