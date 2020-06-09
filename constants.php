1ea7fb70c3e8ba177db612e27889c9a18fe4c410
estevaojneto <estevao.jadanhi@gmail.com>
2020-06-09 12:24:46 -0400

    Finish PSR2; add German language; finish.

--- constants.php
+++ constants.php
@@ -1,4 +1,7 @@
 <?php
-
+/**
+ * This defines some useful constants for our plugin, mostly related to paths.
+ * This must be declared separately from main.php to comply with PSR-2.
+ */
 define('BEC_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('BEC_PLUGIN_URL', plugin_dir_url(__FILE__));
